9d30d313bc75a6232598227a3683aa6ff60623a5
Stef Boerjan <stef.boerjan@gmail.com>
2026-04-26 14:24:07 +0200

    Swapped angular with vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-import { AppModule } from './app/app.module';
+import App from './App.vue'
+import router from './router'
 
+const app = createApp(App)
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
